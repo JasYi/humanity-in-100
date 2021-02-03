c5a95211ab890795af33b3cea63188b8bf320ca1
ThePndaXpres <44511533+ThePndaXpres@users.noreply.github.com>
2021-02-02 20:59:09 -0500

    version 2 with sql server up

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var hbs = require('hbs');
 var path = require('path');
+var mysql = require('mysql');
 
 //form submits to seperate helper page which is defined in main js file which does the sql stuff then
 //helper page then goes back to map page
@@ -10,6 +11,14 @@ var app = express();
 app.set('port', process.env.PORT || 8080);
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'static')));
+
+//SQL setup
+var sql_params = {
+  connectionLimit : 10, //replace with info from the site
+  user            : 'b2335052490792',
+  password        : 'c4e29ae3',
+  host            : 'us-cdbr-east-03.cleardb.com',
+}
 
 app.get('/', function(req, res){
     res.render('index');
@@ -22,3 +31,12 @@ app.get('/form', function(req, res){
 var listener = app.listen(app.get('port'), function() {
   console.log( 'Express server started on port: '+listener.address().port );
 });
+
+
+mysql://
+
+:
+c4e29ae3
+@
+us-cdbr-east-03.cleardb.com
+/heroku_24a377e027a75d5?reconnect=true
---
+++ setup.sql
@@ -0,0 +1,3 @@
+DROP TABLE IF EXISTS stories;
+CREATE TABLE stories
+(id INT, p_name VARCHAR(100), country VARCHAR(100), message VARCHAR(1000), PRIMARY KEY(id));
