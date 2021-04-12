25fee3ed5d82f38eeef5daba13a799b17dc096c0
ThePndaXpres <44511533+ThePndaXpres@users.noreply.github.com>
2021-04-12 18:23:44 -0400

    static stuff

--- static/js/form.js
+++ static/js/form.js
@@ -4,9 +4,9 @@ function send(){
     "country": document.getElementById("Country").value,//this is an array of 2 objects
     "story": document.getElementById("story").value
   }
-
+  
   var ajax_params = {
     'url': 'localhost:8080/background_worker',
-    
+
   }
 }
